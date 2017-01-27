7ad6d9aae485aeff768b92eb135fbdb6d102fbee
Rasmus Fløe <rafl@dr.dk>
2017-01-27 16:25:26 +0100

    add scoped function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
-function bem (...args) {
-	return argParser(...args).join(" ");
+const bem = (...args) => argParser(false, ...args).join(" ");
+
+bem.single = (...args) => argParser(true, ...args).pop();
+
+bem.scoped = (...args) => argParser(true, ...args).join(" ");
+
+module.exports = bem;
+
+function argParser (scoped, block, ...args) {
+	return args.reduce(argReducer(scoped), [block]);
 }
 
-bem.single = (...args) => argParser(...args).pop();
+function argReducer (scoped) {
 
-function argParser (block, ...args) {
-	return args.reduce(argReducer, [block]);
-}
+	return function (result, value) {
+		if (typeof value === "object") {
+			return modifers(result, value);
+		}
 
-function argReducer (result, value) {
+		var prefixed = prefix(result, `__${value}`);
 
-	if (typeof value === "object") {
-		return result.concat(
-			modifierParser(value)
-			.reduce(
-				(subresult, modifier) => subresult.concat(prefix(result, modifier)),
-				[]
-			)
-		);
+		return (scoped) ? prefixed : [value].concat(prefixed);
 	}
 
-	return [value].concat(prefix(result, `__${value}`));
+}
 
+function modifers(result, value) {
+	return result.concat(
+		modifierParser(value)
+		.reduce(
+			(subresult, modifier) => subresult.concat(prefix(result, modifier)),
+			[]
+		)
+	);
 }
 
 function modifierParser (modifiers) {
@@ -45,5 +55,3 @@ function prefix (prefixes, value) {
 		(prefix) => `${prefix}${value}`
 	);
 }
-
-module.exports = bem;
